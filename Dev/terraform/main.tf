7f7ddc9701a6b2841ce9eec7fbd24c3f1a4abc2d
fikunmi samson <adejaresamson42@gmail.com>
2023-07-03 10:54:00 +0100

    fixed provider block

--- Dev/terraform/main.tf
+++ Dev/terraform/main.tf
@@ -5,13 +5,15 @@ terraform {
     workspaces {
       tags = [ "local" ]
     }
-
+  }
+  required_providers {
     aws = {
       source = "hashicorp/aws"
-      version = "~> 4.67.0"
+      version = "4.67.0"
     }
   }
 }
+
 
 provider "aws" {
   region     = var.AWS_REGION
